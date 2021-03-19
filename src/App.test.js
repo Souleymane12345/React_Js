542665a46aee99d04ad62b7b7ce3db04c0dc7926
Souleymane12345 <souleymanedabonepro123@gmail.com>
2021-03-19 12:13:37 +0100

    Initialisation du repertoire

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { expect } from 'chai'
+import React from 'react'
+import { shallow } from 'enzyme'
+import sinon from 'sinon'
+import App from './App'
+import GuessCount from './GuessCount'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import Card from './Card'
+
+describe('<Card/>', () => {
+  it('should trigger its `onClick` prop when clicked', () => {
+    const onClick = sinon.spy()
+    const wrapper = shallow(
+      <Card card="😁" feedback="hidden" index={0} onClick={onClick} />
+    )
+
+    wrapper.simulate('click')
+    expect(onClick).to.have.been.calledWith(0)
+  })
+})
+
+
+
+
+describe ('<App />', () => {
+    it ('renders without crashing ', () => {
+        const wrapper = shallow (<App />)
+
+        expect(wrapper).to.contain( <GuessCount guesses={0} />)
+    })
+
+    it ('has 36 cards', () => {
+        const wrapper = shallow(<App />)
+        expect(wrapper.find('Card')).to.have.length(36)
+    })
+})
